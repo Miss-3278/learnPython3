bcea266a593ba3b39bb3edac325ee44e9fea25b8
lixun <lixun3278@gmai.com>
2017-08-05 07:04:29 +0800

    return double

--- ex10-1.py
+++ ex10-1.py
@@ -1,7 +1,8 @@
 #! /urs/bin/env python
 # coding=utf-8
 
-# 不停地换行打印五种符号，按 command+D 结束
+# 不停地换行打印五种符号，按 command+D 停止
+
 while True:
     for i in ["/", "-", "|", "\\", "|"]:
         print("%s\r" % i) # \r 回车符
